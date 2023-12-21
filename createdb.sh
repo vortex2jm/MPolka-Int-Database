009472006a4299ec094d85ca56ab311804cc4bfb
vortex2jm <joaopaulomoura5328@gmail.com>
2023-12-21 11:39:59 -0300

    :recycle: Improving script

--- createdb.sh
+++ createdb.sh
@@ -1,4 +1,5 @@
 docker run \
+      --name influx_db -d \
       -p 8086:8086 \
-      -v myInfluxVolume:/var/lib/influxdb2 \
+      -v /home/vortex2jm/dev/python/mpolka-int-database/influxdb_volume:/var/lib/influxdb2 \
       influxdb:latest
