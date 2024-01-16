43167f0ee7eec65395a487f5ebe88c1cdeebc554
João Paulo Moura Clevelares <joaopaulomoura5328@gmail.com>
2024-01-16 16:24:57 -0300

    :truck: Renaming volume path

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -1,5 +1,5 @@
 docker run \
       --name influx_db -d \
       -p 8086:8086 \
-      -v /home/vortex2jm/dev/python/mpolka-int-database/influxdb_volume:/var/lib/influxdb2 \
+      -v /home/joao/Code/ic/MPolka-Int-Database/influxdb_volume:/var/lib/influxdb2 \
       influxdb:latest
