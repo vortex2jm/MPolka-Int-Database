008be23e2c91f03659b7db6a67c5b7eec27a8569
João Paulo Moura Clevelares <joaopaulomoura5328@gmail.com>
2024-01-16 16:25:50 -0300

    :sparkles: Adding functions and data flow for solution 1

--- lib/data_parsing.py
+++ lib/data_parsing.py
@@ -1,1 +1,27 @@
-import pandas
+from influxdb_client import Point
+import pandas as pd
+
+# Testing ...
+# points = []
+# df_e1 = pd.read_csv('../data/solucao1/log_INT_E1.csv')    
+# print(df_e1)
+
+# Function for solution 1 =========================
+def sol1_parse(path):
+    points = []
+    df = pd.read_csv(path)
+    for index, row in df.iterrows():
+        # print(str(row['switchID_t']), row[' egress_port'], row[' qid'], row[' enq_qdepth'])
+        p = Point('IntData').tag('Switch', str(row['switchID_t'])).tag('port', str(row[' egress_port']))\
+        .tag('queue', str(row[' qid'])).field('depth', int(row[' enq_qdepth']))
+    
+        points.append(p)
+    return points
+
+# Function for solution 2 =========================
+def sol2_parse(path):
+    df = pd.read_csv(path)
+
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from influxdb_client import InfluxDBClient, WritePrecision, Point
+from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from dotenv import load_dotenv
+from lib.data_parsing import sol1_parse, sol2_parse
 import os
 
 load_dotenv()
 
 url = 'http://localhost:8086'
 token = os.getenv('TOKEN')
-org = 'MPolkaInt'
-bucket = 'IntData'
+org = 'mpolkaint'
+bucket = 'sol1'
 
-# Instantiating the db client 
-client = InfluxDBClient(url=url, token=token, org=org)
+def main():
+    # Instantiating the db client 
+    client = InfluxDBClient(url=url, token=token, org=org)
 
-# Creating writing api
-write_api = client.write_api(write_options=SYNCHRONOUS)
+    # Creating writing api
+    write_api = client.write_api(write_options=SYNCHRONOUS)
 
-# Creating data point
-point = Point('Connectivity').tag('port', 'eth0').field('latency', 23)
+    for point in sol1_parse('./data/solucao1/log_INT_E1.csv'):
+        # Writing data
+        write_api.write(org=org, bucket=bucket, record=point, write_precision=WritePrecision.MS)
 
-# Writing data
-write_api.write(org=org, bucket=bucket, record=point)
+if __name__ == '__main__':
+    main()
